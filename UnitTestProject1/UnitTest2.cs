70024e084b955c9bcf03407dcd0ca90e881effef
vbalaja <jagan.balasubramani@pearson.com>
2017-08-08 20:58:25 +0530

    Commit

---
+++ UnitTestProject1/UnitTest2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.Fixtures)]
+    [Category("Test2")]
+    public class UnitTest2
+    {
+        public string classname;
+        [SetUp]
+        public void setup()
+        {
+            classname = "two";
+            String name = TestContext.CurrentContext.Test.Name;
+            String Methodname = TestContext.CurrentContext.Test.MethodName;
+            String ID = TestContext.CurrentContext.Test.ID;
+            String FullName = TestContext.CurrentContext.Test.FullName;
+        }
+
+        [TearDown]
+        public void teardown()
+        {
+            Debug.WriteLine("Teardown");
+            if( TestContext.CurrentContext.Result.Outcome.Equals(ResultState.Failure)){
+                Debug.WriteLine("failure count "+TestContext.CurrentContext.Result.FailCount);
+            } else if( TestContext.CurrentContext.Result.Outcome.Equals(ResultState.Success)){
+                Debug.WriteLine("failure count "+TestContext.CurrentContext.Result.PassCount);
+            }
+        }
+
+        [Test]
+        public void TestMethod1()
+        {
+            Thread.Sleep(2000);
+            Debug.WriteLine(classname + " and Method 1");
+        }
+        [Test]
+        public void TestMethod2()
+        {
+            Thread.Sleep(2000);
+            Debug.WriteLine(classname + " and Method 2");
+        }
+        [Test]
+        public void TestMethod3()
+        {
+            Thread.Sleep(2000);
+            Debug.WriteLine(classname + " and Method 3");
+        }
+        [Test]
+        public void TestMethod4()
+        {
+            Thread.Sleep(2000);
+            Debug.WriteLine(classname + " and Method 4");
+        }
+    }
+}
--- UnitTestProject1/UnitTest4.cs
+++ UnitTestProject1/UnitTest4.cs
@@ -1,42 +1,38 @@
 ﻿using System;
+using NUnit.Framework;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Threading;
 using System.Diagnostics;
-using NUnit.Framework;
 
 namespace UnitTestProject1
 {
-    [TestFixture]
-    [Parallelizable(ParallelScope.Children)]
-    [Category ("Test1")]
-    public class UnitTest1
+    //[TestFixture]
+    //[Parallelizable(ParallelScope.Fixtures)]
+    public class UnitTest4
     {
         [Test]
-        
         public void TestMethod1()
         {
             Thread.Sleep(2000);
-            Debug.WriteLine("Test 1 and Method 1");
+            Debug.WriteLine("Test 4 and Method 1");
         }
         [Test]
-      
         public void TestMethod2()
         {
             Thread.Sleep(2000);
-            Debug.WriteLine("Test 1 and Method 2");
+            Debug.WriteLine("Test 4 and Method 2");
         }
         [Test]
-       
         public void TestMethod3()
         {
             Thread.Sleep(2000);
-            Debug.WriteLine("Test 1 and Method 3");
+            Debug.WriteLine("Test 4 and Method 3");
         }
         [Test]
         public void TestMethod4()
         {
             Thread.Sleep(2000);
-            Debug.WriteLine("Test 1 and Method 4");
+            Debug.WriteLine("Test 4 and Method 4");
         }
     }
 }
