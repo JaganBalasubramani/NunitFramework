c9df09017733357a92433e7b2d3d48bada66e060
vbalaja <jagan.balasubramani@pearson.com>
2017-08-09 20:13:16 +0530

    branch commit

---
+++ UnitTestProject1/UnitTest5.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NUnit.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+using System.Diagnostics;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.Fixtures)]
+    public class UnitTest5
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Thread.Sleep(2000);
+            Debug.WriteLine("Test 5 and Method 1");
+        }
+        [Test]
+        public void TestMethod2()
+        {
+            Thread.Sleep(2000);
+            Debug.WriteLine("Test 5 and Method 2");
+        }
+        [Test]
+        public void TestMethod3()
+        {
+            Thread.Sleep(2000);
+            Debug.WriteLine("Test 5 and Method 3");
+        }
+        [Test]
+        public void TestMethod4()
+        {
+            Thread.Sleep(2000);
+            Debug.WriteLine("Test 5 and Method 4");
+        }
+    }
+}
