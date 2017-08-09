8aad1cf1b9871187e6cb307a555c2aea7bc9e92f
vbalaja <jagan.balasubramani@pearson.com>
2017-08-09 19:41:16 +0530

    Unittest3 commit

---
+++ UnitTestProject1/UnitTest3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+using System.Diagnostics;
+using NUnit.Framework;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.Fixtures)]
+    public class UnitTest3
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Thread.Sleep(2000);
+            Debug.WriteLine("Test 3 and Method 1");
+        }
+        [Test]
+        public void TestMethod2()
+        {
+            Thread.Sleep(2000);
+            Debug.WriteLine("Test 3 and Method 2");
+        }
+        [Test]
+        public void TestMethod3()
+        {
+            Thread.Sleep(2000);
+            Debug.WriteLine("Test 3 and Method 3");
+        }
+        [Test]
+        public void TestMethod4()
+        {
+            Thread.Sleep(2000);
+            Debug.WriteLine("Test 3 and Method 4");
+        }
+    }
+}
